523afa88ab62b652442cd6224832798f5bdac318
Gary Coleman <gryclmn@hotmail.com>
2019-08-08 07:05:12 -0700

    Assignment completed.

---
+++ src/App.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * ********************
+		 * Step 1
+		 * ********************
+		 */
+		List<String> employeeNames = new ArrayList<String>(); 
+
+		/* 
+		 * ********************
+		 * Step 2
+		 * ********************
+		 */
+		Set<Integer> ids = new HashSet<Integer>();
+		
+		/* 
+		 * ********************
+		 * Step 3
+		 * ********************
+		 */
+		Map<Integer, String> employeeMap = new HashMap<Integer, String>();
+		
+		/* 
+		 * ********************
+		 * Step 4
+		 * ********************
+		 */
+		employeeNames.add("Mario");
+		employeeNames.add("Luigi");
+		employeeNames.add("Peach");
+		employeeNames.add("Toad");
+		employeeNames.add("Bowser");
+		for (int i = 1; i <= employeeNames.size(); i++) {
+			ids.add(i);
+		}
+		
+		/* 
+		 * ********************
+		 * Step 5
+		 * ********************
+		 */
+		int i = 0;
+		for (Integer id : ids) {
+			employeeMap.put(id, employeeNames.get(i));
+			i++;
+		}
+		
+		/* 
+		 * ********************
+		 * Step 6
+		 * ********************
+		 */
+		for (Integer key : employeeMap.keySet()) {
+			System.out.printf("Employee %d is %s.%n", key, employeeMap.get(key));
+		}
+		
+		/* 
+		 * ********************
+		 * Step 7
+		 * ********************
+		 */
+		StringBuilder idsBuilder = new StringBuilder();
+		
+		/* 
+		 * ********************
+		 * Step 8
+		 * ********************
+		 */
+		for (Integer id : ids) {
+			idsBuilder.append(id + "-");
+		}
+		
+		/* 
+		 * ********************
+		 * Step 9
+		 * ********************
+		 */
+		System.out.println(idsBuilder.toString());
+		
+		/* 
+		 * ********************
+		 * Step 10
+		 * ********************
+		 */
+		StringBuilder namesBuilder = new StringBuilder();
+		
+		/* 
+		 * ********************
+		 * Step 11
+		 * ********************
+		 */
+		for (String name : employeeNames) {
+			namesBuilder.append(name + " ");
+		}
+		
+		/* 
+		 * ********************
+		 * Step 12
+		 * ********************
+		 */
+		System.out.println(namesBuilder.toString());
+	}
+
+}
